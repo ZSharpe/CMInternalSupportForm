e16538e50d820949aa03739752f4de35919fe32c
Zach <zach.sharpe5@gmail.com>
2019-02-28 09:04:17 -0600

    Add deeper commenting in JS file. Change selector functionality in selector change listener to directly alter href attribute in resource links. Disable resizing on description box.

--- internalSupportForm.js
+++ internalSupportForm.js
@@ -2,30 +2,33 @@
 
 var option = $("#recordType option:selected");
 var description = $("#description")[0];
+var checkResources = $("#checkResources")[0];
 var resources = $("#resources");
-var listItems = $("li");
-listItems.hide();
+var listItemsAll = $("li");
+var rc = $("#rc")[0];
 
+listItemsAll.hide();
+checkResources.textContent = "";
+
+// Record Type change selection event listener to alter links and description area based on selection.
 $('#recordType').change(function() {
     option = $("#recordType option:selected")[0];
 
-    // If Campaign Monitor is selected in recordType
+    // If Campaign Monitor is selected in recordType, change description and alter resource link 2.
     if(option.value === "012o00000016Bio"){
-        listItems.show();
-        listItems[1].innerHTML(
-            "<a href=\"https://help.campaignmonitor.com/\">Resource Center</a>"
-        );
+        listItemsAll.show();
+        rc.setAttribute("href", "https://help.campaignmonitor.com/")
         description.value = "- Who should we follow up with (you or the customer)?: \n- Customer Name & Email Address: \n- Global ID or Hex Link:\n- Your Name & Email address: \n- Your Department: \n- Description of Issue:";
-    // If Emma is selected in recordType
+    // If Emma is selected in recordType, change description and alter resource link 2.
     }else if(option.value === "0121J000000yZ1A"){
-        listItems.show();
-        listItems[1].innerHTML(
-            "<a href=\"https://support.e2ma.net/Resource_Center\">Resource Center</a>"
-        );
+        listItemsAll.show();
+        rc.setAttribute("href", "https://support.e2ma.net/Resource_Center")
         description.value = "- Who should we follow up with (you or the customer)?: \n- Username or Followup Email Address: \n- Your Department: \n- Description of Issue: \n- Sub Account ID (where applicable):";
+    // If nothing is selected or "--None--", hide resource list and empty description field.
     }else{
+        checkResources.textContent = "";
         description.value = "";
-        listItems.hide();
+        listItemsAll.hide();
     }
 
 });
