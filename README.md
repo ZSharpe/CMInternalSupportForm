2d415de54a875568d2a7c3600b31ecd92576e525
Zach <zach.sharpe5@gmail.com>
2019-02-28 11:52:28 -0600

    Addition of README.md describing prerequisites, caveats, tools used, and credits

---
+++ README.md
@@ -0,0 +1,23 @@
+# Internal Support Form
+
+This form allows employees of Campaign Monitor and Emma products to submit an internal help request with the relevant support team. This tool is implemented to unify process across both brands post-merge.
+
+### Prerequisites
+
+This project will be hosted on an internal domain for Emma / CM use exclusively. The files can be cloned and viewed locally.
+
+## Built With
+
+* Javascript
+* jQuery
+* Bootstrap 4
+* HTML 5
+* CSS 3
+
+## Authors
+
+* Zach Sharpe (https://github.com/zsharpe, https://zachsharpe.net)
+
+## Acknowledgments
+
+* Credit to the awesome ReadMe template provided by PurpleBooth @ https://gist.github.com/PurpleBooth/109311bb0361f32d87a2
--- internalSupportForm.js
+++ internalSupportForm.js
@@ -19,24 +19,32 @@ $('#recordType').change(function() {
     if(option.value === "012o00000016Bio"){
         // Show resource link list
         listItemsAll.show();
+
         // Enable submit button, change background color of submit button to reflect Campaign Monitor coloring
         $("#submit").prop("disabled", false);
         $("#submit").css("background-color", "#7956FF");
+
         // Update href of "resource center" link to Campaign Monitor resources.
         rc.setAttribute("href", "https://help.campaignmonitor.com/")
+
         // Update description field with CM relevant questions
         description.value = "- Who should we follow up with (you or the customer)?: \n- Customer Name & Email Address: \n- Global ID or Hex Link:\n- Your Name & Email address: \n- Your Department: \n- Description of Issue:";
     // If Emma is selected in recordType, change description and alter resource link 2.
     }else if(option.value === "0121J000000yZ1A"){
+
         // Show resource link list
         listItemsAll.show();
+
         // Enable submit button, change background color of submit button to reflect Campaign Monitor coloring
         $("#submit").prop("disabled", false);
         $("#submit").css("background-color", "#509DF5");
+
         // Update href of "resource center" link to Emma resources.
         rc.setAttribute("href", "https://support.e2ma.net/Resource_Center")
+        
         // Update description field with Emma relevant questions
         description.value = "- Who should we follow up with (you or the customer)?: \n- Username or Followup Email Address: \n- Your Department: \n- Description of Issue: \n- Sub Account ID (where applicable):";
+        
     // If nothing is selected or "--None--", hide resource list and empty description field.
     }else{
         $("#submit").prop("disabled", true);
